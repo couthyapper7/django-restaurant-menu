abee115128d9a70434403383e874d47b101d9b4e
fausto <couthyapper79@gmail.com>
2024-08-23 18:14:23 -0300

    base functions

--- main/models.py
+++ main/models.py
@@ -3,21 +3,23 @@ from django.contrib.auth.models import User
 import os
 import uuid
 
-def category_image_upload_path(instance, filename):
+def business_image_upload_path(instance, filename, folder):
     ext = filename.split('.')[-1]
-    filename = f"{instance.name}.{ext}"
-    return os.path.join('category', filename)
+    filename = f"{instance.name}-{folder}.{ext}"
+    return os.path.join('business', instance.url_identifier, folder, filename)
 
-def item_image_upload_path(instance, filename):
-    ext = filename.split('.')[-1]
-    filename = f"{instance.name}.{ext}"
-    return os.path.join('item_images', filename)
+def banner_upload_path(instance, filename):
+    return business_image_upload_path(instance, filename, 'banners')
 
+def icon_upload_path(instance, filename):
+    return business_image_upload_path(instance, filename, 'icons')
 
 class Business(models.Model):
     name = models.CharField(max_length=100)
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     url_identifier = models.CharField(max_length=36, unique=True, blank=True, editable=False)
+    banner = models.ImageField(upload_to=banner_upload_path, null=True, blank=True)
+    icon = models.ImageField(upload_to=icon_upload_path, null=True, blank=True)
 
     def save(self, *args, **kwargs):
         if not self.url_identifier:
@@ -27,15 +29,27 @@ class Business(models.Model):
     def __str__(self):
         return self.name
 
+def category_image_upload_path(instance, filename):
+    ext = filename.split('.')[-1]  # Get the file extension
+    filename = f"{instance.name}.{ext}"  # Create a new filename with the category's name
+    return os.path.join('category', instance.business.name, instance.business.url_identifier, filename)
+
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
     business = models.ForeignKey(Business, on_delete=models.CASCADE, related_name='categories')
-    image = models.ImageField(upload_to=category_image_upload_path, null=False, blank=False, default='src/images/default/image.png')
+    image = models.ImageField(upload_to=category_image_upload_path, null=False, blank=False, default='src/static/images/default/image.png')
 
     def __str__(self):
         return self.name
+
+
+
+def item_image_upload_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f"{instance.name}.{ext}"
+    return os.path.join('item_images', filename)
 
 class Item(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='items')
--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import BusinessDetailView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('<str:url_identifier>/', BusinessDetailView.as_view(), name='business-detail'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
